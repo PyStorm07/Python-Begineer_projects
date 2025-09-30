5907509b3e05883db8813d3de53910ed400c08f3
Aryan Sinha <160517983+PyStorm07@users.noreply.github.com>
2025-09-30 21:30:53 +0530

    Tip Calculator Project Day 2

--- task.py
+++ task.py
@@ -1,4 +1,8 @@
-print("Welcome to the Band Name Generator")
-city = input("Whats the name of the city you grew up in?\n")
-pet = input("Whats your Pet Name ?\n")
-print("Your Band Name Could be: " + " " + city + " " + pet)
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+bill_with_tip = tip/100 * bill + bill
+final_bill = round(bill_with_tip / people, 2)
+print(f"Each Should Pay: ${final_bill}")
+
